ec473b38c5844df4ed3b52469d0162d9a903a7a4
skmm6 <egor.qaq@gmail.com>
2023-03-22 12:51:20 +0300

    ошибка Cannot read properties of null (reading useState

--- app.jsx
+++ app.jsx
@@ -1,6 +1,12 @@
 import { createRoot } from 'react-dom/client';
+import { useState } from 'react';
+
 document.body.innerHTML = '<div id="app"></div>';
 const root = createRoot(document.getElementById('app'));
+
+if (module.hot) {
+  module.hot.accept();
+}
 /*
 const user = {
   name: 'Hedy Lamarr',
@@ -38,7 +44,7 @@ export default function MyApp() {
 }
 
 root.render(MyApp());
-*/
+
 const user = {
   name: 'Hedy Lamarr',
   imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
@@ -61,5 +67,134 @@ export default function Profile() {
     </>
   );
 }
+*/
 
-root.render(Profile());
+// ====================================Условный рендер TODO==========================
+// ====================================Условный рендер TODO==========================
+// ====================================Условный рендер TODO==========================
+/*
+let content;
+let isLoggedIn = true;
+
+function AdminPanel() {
+  return (
+    <>
+      <h1>Админ панель</h1>
+    </>
+  );
+}
+function LoginForm() {
+  return (
+    <>
+      <h1>Логин</h1>
+    </>
+  );
+}
+
+// function RenderContent() {
+//   if (isLoggedIn) {
+//     content = <AdminPanel />;
+//     console.log('1');
+//   } else {
+//     content = <LoginForm />;
+//   }
+// }
+
+function ToggleButton() {
+  if (isLoggedIn) {
+    isLoggedIn = false;
+  } else {
+    isLoggedIn = true;
+  }
+
+  root.render(RenderYsl());
+}
+
+function RenderYsl() {
+  return (
+    <div>
+      {isLoggedIn ? <AdminPanel /> : <LoginForm />}
+      <button onClick={ToggleButton}>Переключатель</button>
+    </div>
+  );
+}
+
+// function RenderYsl() {
+//   return (
+//     <div>
+//       {isLoggedIn && <AdminPanel />}
+//       <button onClick={ToggleButton}>Переключатель</button>
+//     </div>
+//   );
+// }
+
+
+
+
+*/
+
+// ====================================Визуализация списков  TODO==========================
+// ====================================Визуализация списков  TODO==========================
+// ====================================Визуализация списков  TODO==========================
+/*
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 1 },
+  { title: 'Apple', isFruit: true, id: 1 },
+];
+
+export default function ShoppingList() {
+  const listItems = products.map(product => (
+    <li
+      key={product.id}
+      style={{ color: product.isFruit ? 'magenta' : 'darkgreen' }}
+    >
+      {product.title}
+    </li>
+  ));
+
+  return <ul>{listItems}</ul>;
+}
+
+root.render(ShoppingList());
+
+// ====================================Реагирование на события   TODO==========================
+// ====================================Реагирование на события   TODO==========================
+// ====================================Реагирование на события   TODO==========================
+/*
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+  return <button onClick={handleClick}>Click me</button>;
+}
+
+root.render(MyButton());
+*/
+// ====================================Обновление экрана  TODO==========================
+// ====================================Обновление экрана  TODO==========================
+// ====================================Обновление экрана  TODO==========================
+
+// import { useState } from 'react';
+
+// function MyButton() {
+//   const [count, setCount] = useState(0);
+
+//   function handleClick() {
+//     setCount(count + 1);
+//   }
+
+//   return <button onClick={handleClick}> Clicked {count} times</button>;
+// }
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return <button onClick={handleClick}>Clicked {count} times</button>;
+}
+
+root.render(MyButton());
