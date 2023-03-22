6fd11830b4b6337852c1f3b2a2f669fb1b0f72ad
skmm6 <egor.qaq@gmail.com>
2023-03-22 20:40:54 +0300

    update

--- index.js
+++
@@ -1,8 +0,0 @@
-// import { createRoot } from 'react-dom/client';
-
-// // Очищаем существующий HTML элемент
-// document.body.innerHTML = '<div id="app"></div>';
-
-// // Вместо этого визуализируйте свой компонент React
-// const root = createRoot(document.getElementById('app'));
-// root.render(<h1>Hello, world</h1>);
