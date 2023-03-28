58926afef0df3e3f834b401a5eb2627703537073
Alacosta Alacosta <egor.qaq@gmail.com>
2023-03-28 21:17:26 +0300

    Реагирование на ввод с помощью состояния11

---
+++ ReactDev/ManagingState/src/components/ReactingInputWithState.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+
+export default function ReactingInputWithState() {
+  return (
+    <>
+      <Form1 />
+    </>
+  );
+}
+
+export function Form1() {
+  const [answer, setAnswer] = useState("");
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("typing");
+
+  if (status === "success") {
+    return <h1>Thats right!</h1>;
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("submitting");
+    try {
+      await submitForm(answer);
+      setStatus("success");
+    } catch (err) {
+      setStatus("typing");
+      setError(err);
+    }
+  }
+
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
+  }
+
+  return (
+    <>
+      <h2>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === "submitting"}
+        />
+        <br />
+        <button disabled={answer.length === 0 || status === "submitting"}>
+          Submit
+        </button>
+        {error !== null && <p className="Erorr">{error.message}</p>}
+      </form>
+    </>
+  );
+}
+
+function submitForm(answer) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== "lima";
+      if (shouldError) {
+        reject(new Error("Good guess but a wrong answer. Try again!"));
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+// let statuses = ["empty", "typing", "submitting", "success", "error"];
+
+// export function Forms() {
+//   return (
+//     <>
+//       {statuses.map((status) => (
+//         <section key={status}>
+//           <h4>Form ({status}):</h4>
+//           <Form1 status={status} />
+//         </section>
+//       ))}
+//     </>
+//   );
+// }
